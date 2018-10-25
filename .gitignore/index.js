810ec34075e3d7fb8531ba7e7aea56507a0772f9
StevenGotoGitHub <44396651+StevenGotoGitHub@users.noreply.github.com>
2018-10-25 15:22:20 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,120 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+client.on('message', message => { 
+    if (message.content === '!avatar') {
+        message.reply(message.author.avatarURL);
+    }
+  });
+
+  client.on('message', message => {
+    // Ignore messages that aren't from a guild
+    if (!message.guild) return;
+  
+    // If the message content starts with "!kick"
+    if (message.content.startsWith('!kick')) {
+      // Assuming we mention someone in the message, this will return the user
+      // Read more about mentions over at https://discord.js.org/#/docs/main/stable/class/MessageMentions
+      const user = message.mentions.users.first();
+      // If we have a user mentioned
+      if (user) {
+        // Now we get the member from the user
+        const member = message.guild.member(user);
+        // If the member is in the guild
+        if (member) {
+          /**
+           * Kick the member
+           * Make sure you run this on a member, not a user!
+           * There are big differences between a user and a member
+           */
+          member.kick('Optional reason that will display in the audit logs').then(() => {
+            // We let the message author know we were able to kick the person
+            message.reply(`Successfully kicked ${user.tag}`);
+          }).catch(err => {
+            // An error happened
+            // This is generally due to the bot not being able to kick the member,
+            // either due to missing permissions or role hierarchy
+            message.reply('I was unable to kick the member');
+            // Log the error
+            console.error(err);
+          });
+        } else {
+          // The mentioned user isn't in this guild
+          message.reply('That user isn\'t in this guild!');
+        }
+      // Otherwise, if no user was mentioned
+      } else {
+        message.reply('You didn\'t mention the user to kick!');
+      }
+    }
+  });
+
+  client.on('message', message => {
+    // Ignore messages that aren't from a guild
+    if (!message.guild) return;
+  
+    // if the message content starts with "!ban"
+    if (message.content.startsWith('!ban')) {
+      // Assuming we mention someone in the message, this will return the user
+      // Read more about mentions over at https://discord.js.org/#/docs/main/stable/class/MessageMentions
+      const user = message.mentions.users.first();
+      // If we have a user mentioned
+      if (user) {
+        // Now we get the member from the user
+        const member = message.guild.member(user);
+        // If the member is in the guild
+        if (member) {
+          /**
+           * Ban the member
+           * Make sure you run this on a member, not a user!
+           * There are big differences between a user and a member
+           * Read more about what ban options there are over at
+           * https://discord.js.org/#/docs/main/stable/class/GuildMember?scrollTo=ban
+           */
+          member.ban({
+            reason: 'They were bad!',
+          }).then(() => {
+            // We let the message author know we were able to ban the person
+            message.reply(`Successfully banned ${user.tag}`);
+          }).catch(err => {
+            // An error happened
+            // This is generally due to the bot not being able to ban the member,
+            // either due to missing permissions or role hierarchy
+            message.reply('I was unable to ban the member');
+            // Log the error
+            console.error(err);
+          });
+        } else {
+          // The mentioned user isn't in this guild
+          message.reply('That user isn\'t in this guild!');
+        }
+      } else {
+      // Otherwise, if no user was mentioned
+        message.reply('You didn\'t mention the user to ban!');
+      }
+    }
+  });
+
+client.on('message', message => {
+  if (message.content === '!Help') {
+    message.channel.send('Commandes Info : kick , ban , hi , avatar , info , invit . Prefix : !');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === '!info') {
+    message.channel.send('Devloped by : Steven . Prefix : ! . Creat the : 25/10/18');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === '!invit') {
+    message.channel.send('invit the bot : https://discordapp.com/oauth2/authorize?client_id=504939908448780288&permissions=8&scope=bot ');
+  }
+});
+
+client.login('NTA0OTM5OTA4NDQ4NzgwMjg4.DrMWwg.FmP4hraWVPmjZmWlXeUF_Bnl_2k');
